c44734d8f6112d20a75d13fd6a6342161bbf9294
Muhammad Hamza <muhammadhamza1311@gmail.com>
2022-11-25 18:29:27 +0500

    Revert "/api/run instead of BACKEND_URL"
    This reverts commit 9e257221

--- frontend/src/api/mod.rs
+++ frontend/src/api/mod.rs
@@ -1,1 +1,11 @@
 pub mod share;
+
+pub const BACKEND_URL: &str = option_env!("BACKEND_URL").unwrap_or({
+    #[cfg(debug_assertions)]
+    const DEFAULT: &str = "http://localhost:3000";
+
+    #[cfg(not(debug_assertions))]
+    const DEFAULT: &str = "https://api.play.yew.rs";
+
+    DEFAULT
+});
