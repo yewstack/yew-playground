a0c54de596b2407fdd57d7992bdc03fd5024b242
Muhammad Hamza <muhammadhamza1311@gmail.com>
2022-12-09 14:08:10 +0500

    Fix frontend compile errors

--- frontend/src/api/mod.rs
+++ frontend/src/api/mod.rs
@@ -1,11 +1,14 @@
 pub mod share;
 
-pub const BACKEND_URL: &str = option_env!("BACKEND_URL").unwrap_or({
-    #[cfg(debug_assertions)]
-    const DEFAULT: &str = "http://localhost:3000/api";
+pub const BACKEND_URL: &str = match option_env!("BACKEND_URL") {
+    Some(v) => v,
+    None => {
+        #[cfg(debug_assertions)]
+        const DEFAULT: &str = "http://localhost:3000/api";
 
-    #[cfg(not(debug_assertions))]
-    const DEFAULT: &str = "https://api.play.yew.rs/api";
+        #[cfg(not(debug_assertions))]
+        const DEFAULT: &str = "https://api.play.yew.rs/api";
 
-    DEFAULT
-});
+        DEFAULT
+    }
+};
